8d6460c4a203814ab010b931151be6fe6d0d87a0
epinedo <epinedo@robotmonkeyllc.com>
2024-01-23 21:17:09 -0500

    added prevent default in restart button

--- server/client/src/GameBoard/Controls.jsx
+++ server/client/src/GameBoard/Controls.jsx
@@ -1,10 +1,12 @@
 import { undo } from './functions.jsx'
 
 const Controls = ({isSolved, setIsSolved}) => {
-    const handleRestart = () => {
+    const handleRestart = (e) => {
     //handleGameReStart()
         // ** This is a temporary fix to the restart issue. Update to track actual stack count**
+        e.preventDefault()
         for (let i = 0; i < localStorage.stackCount+1; i++) {
+            console.log('removing state')
             undo(isSolved, setIsSolved)
         }
         /* while (localStorage.undoStack.length > 250) {
@@ -17,7 +19,7 @@ const Controls = ({isSolved, setIsSolved}) => {
         <div className="controls-container">
             <button id="Return" className="control thirds" onClick={() => window.location.reload()}>Return</button>
             <button id="Undo" className="control thirds" onClick={() => undo(isSolved, setIsSolved)}>Undo</button>
-            <button id="Restart" className="control thirds" onClick={() => {handleRestart()}}>Restart</button>
+            <button id="Restart" className="control thirds" onClick={(e) => {handleRestart(e)}}>Restart</button>
         </div>
     )
     
